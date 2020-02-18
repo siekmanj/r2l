6580ff216f818ef2db0f91e76ac70d9e3b530b6d
Jonah Siekmann <siekmanj@oregonstate.edu>
2020-02-17 20:27:00 -0800

    works?

--- algos/qbn.py
+++ algos/qbn.py
@@ -54,7 +54,7 @@ def collect_data(actor, timesteps, max_traj_len, seed):
 
     return [states, actions, hiddens, cells]
 
-def eval_qbn(actor, obs_qbn, hid_qbn, cel_qbn, episodes=10, max_traj_len=1000):
+def evaluate(actor, obs_qbn=None, hid_qbn=None, cel_qbn=None, episodes=10, max_traj_len=1000):
   with torch.no_grad():
     env = env_factory(actor.env_name)()
     reward = 0
@@ -73,9 +73,14 @@ def eval_qbn(actor, obs_qbn, hid_qbn, cel_qbn, episodes=10, max_traj_len=1000):
         hidden_state = actor.hidden[0]
         cell_state   = actor.cells[0]
 
-        norm_state   = obs_qbn(norm_state) # discretize state
-        actor.hidden = [hid_qbn(hidden_state)]
-        actor.cells  = [cel_qbn(cell_state)]
+        if obs_qbn is not None:
+          norm_state   = obs_qbn(norm_state) # discretize state
+
+        if hid_qbn is not None:
+          actor.hidden = [hid_qbn(hidden_state)]
+
+        if cel_qbn is not None:
+          actor.cells  = [cel_qbn(cell_state)]
 
         action       = actor(norm_state)
         state, r, done, _ = env.step(action.numpy())
@@ -84,14 +89,14 @@ def eval_qbn(actor, obs_qbn, hid_qbn, cel_qbn, episodes=10, max_traj_len=1000):
     return reward
   
 
-def train_lstm_qbn(policy, states, actions, hiddens, cells, epochs=500, batch_size=64, logger=None):
-  obs_qbn    = QBN(states.shape[-1])
-  hidden_qbn = QBN(hiddens.shape[-1])
-  cell_qbn   = QBN(cells.shape[-1])
+def train_lstm_qbn(policy, states, actions, hiddens, cells, epochs=500, batch_size=128, logger=None):
+  obs_qbn    = QBN(states.shape[-1], layers=(32,64,128))
+  hidden_qbn = QBN(hiddens.shape[-1], layers=(32,64,128))
+  cell_qbn   = QBN(cells.shape[-1], layers=(32,64,128))
 
-  obs_optim = optim.Adam(obs_qbn.parameters(), lr=1e-4, eps=1e-6)
-  hidden_optim = optim.Adam(hidden_qbn.parameters(), lr=1e-4, eps=1e-6)
-  cell_optim = optim.Adam(cell_qbn.parameters(), lr=1e-4, eps=1e-6)
+  obs_optim = optim.Adam(obs_qbn.parameters(), lr=1e-5, eps=1e-6)
+  hidden_optim = optim.Adam(hidden_qbn.parameters(), lr=1e-5, eps=1e-6)
+  cell_optim = optim.Adam(cell_qbn.parameters(), lr=1e-5, eps=1e-6)
 
   for epoch in range(epochs):
     random_indices = SubsetRandomSampler(range(states.shape[0]))
@@ -130,13 +135,15 @@ def train_lstm_qbn(policy, states, actions, hiddens, cells, epochs=500, batch_si
     epoch_obs_losses = np.mean(epoch_obs_losses)
     epoch_hid_losses = np.mean(epoch_hid_losses)
     epoch_cel_losses = np.mean(epoch_cel_losses)
-    reward = eval_qbn(policy, obs_qbn, hidden_qbn, cell_qbn)
-    print("{:5.3f} | {:5.3f} | {:5.3f} | reward: {:5.3f}".format(epoch_obs_losses, epoch_hid_losses, epoch_cel_losses, reward))
+    d_reward = evaluate(policy, obs_qbn=obs_qbn, hid_qbn=hidden_qbn, cel_qbn=cell_qbn)
+    n_reward = evaluate(policy)
+    print("{:7.5f} | {:7.5f} | {:7.5f} | QBN reward: {:5.1f} | nominal reward {:5.1f} ".format(epoch_obs_losses, epoch_hid_losses, epoch_cel_losses, d_reward, n_reward))
     if logger is not None:
-      logger.add_scalar('qbn/obsloss', epoch_obs_losses, epoch)
-      logger.add_scalar('qbn/hidloss', epoch_hid_losses, epoch)
-      logger.add_scalar('qbn/celloss', epoch_cel_losses, epoch)
-      logger.add_scalar('qbn/reward', reward, epoch)
+      logger.add_scalar(policy.env_name + '_qbn/obs_loss', epoch_obs_losses, epoch)
+      logger.add_scalar(policy.env_name + '_qbn/hidden_loss', epoch_hid_losses, epoch)
+      logger.add_scalar(policy.env_name + '_qbn/cell_loss', epoch_cel_losses, epoch)
+      logger.add_scalar(policy.env_name + '_qbn/qbn_reward', d_reward, epoch)
+      logger.add_scalar(policy.env_name + '_qbn/nominal_reward', n_reward, epoch)
 
   return obs_qbn, hidden_qbn, cell_qbn
   
@@ -193,5 +200,5 @@ def run_experiment(args):
   hiddens = torch.as_tensor(data['hiddens'])
   cells   = torch.as_tensor(data['cells'])
 
-  obs_qbn, hid_qbn, cel_qbn = train_lstm_qbn(policy, states, actions, hiddens, cells)
+  obs_qbn, hid_qbn, cel_qbn = train_lstm_qbn(policy, states, actions, hiddens, cells, logger=logger)
 
